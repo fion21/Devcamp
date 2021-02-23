b2b6fc5c8945dd4a1bf0939ac579f789d349614f
Fion L <hwindepth@yahoo.com>
2021-02-23 22:27:54 +0000

    added routes and middleware

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-// Load config vars
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
-dotenv.config({path: './config/config.env'});
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
-
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
